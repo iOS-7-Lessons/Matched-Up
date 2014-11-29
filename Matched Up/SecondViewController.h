92603f766b10cafaafeba8ee0c13f3bd334bec7d
macbook <macbook@macbook-MacBook-Pro.local>
2014-11-29 16:54:22 +0200

    Initial Commit

---
+++ Matched Up/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Matched Up
+//
+//  Created by macbook on 29/11/14.
+//  Copyright (c) 2014 Eray Diler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
